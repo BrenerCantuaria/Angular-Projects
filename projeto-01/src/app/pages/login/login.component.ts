643483518fb30a3b1233f2b3602c04423ed836cb
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-02-04 14:28:09 -0400

    Update: projeto-01 - estrutura de login

--- projeto-01/src/app/pages/login/login.component.ts
+++ projeto-01/src/app/pages/login/login.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+
+/** Personalização de erro para validação */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null): boolean {
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+}
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
 })
 export class LoginComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControl = new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(8)]);
 
+  matcher = new MyErrorStateMatcher();
 }
