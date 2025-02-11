9c657d47f26b1d87b3bfd2b723da8a10691a6068
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-02-11 12:48:50 -0400

    Add: tela de cadastro do usuário

--- projeto-001/src/app/pages/register/register.component.ts
+++ projeto-001/src/app/pages/register/register.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { CommonModule } from '@angular/common'; 
+import { StyleClassModule } from 'primeng/styleclass';
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, InputTextModule, PasswordModule, ButtonModule, FloatLabelModule,CommonModule,StyleClassModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, { validators: this.passwordsMatchValidator });
+  }
+
+  passwordsMatchValidator(form: FormGroup) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordsMismatch: true };
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      // Lógica para processar os dados do formulário
+    }
+  }
 }
