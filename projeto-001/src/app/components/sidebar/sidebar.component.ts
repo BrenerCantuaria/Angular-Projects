9f3ad5e08d31b441a9a48bca7c1e20cf4c9e31a2
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-02-13 12:43:00 -0400

    Create: sidebar

---
+++ projeto-001/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
+import { CommonModule } from '@angular/common';
+import { Menubar } from 'primeng/menubar';
+// PrimeNG
+import { PanelMenuModule } from 'primeng/panelmenu';
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+  imports: [CommonModule, MenuModule,PanelMenuModule],
+})
+export class SidebarComponent {
+  items: MenuItem[] = [];
+
+  constructor() {
+    this.items = [
+      { label: 'Home', icon: 'pi pi-home', routerLink: '/home' },
+      { label: 'Dashboard', icon: 'pi pi-chart-line', routerLink: '/dashboard' },
+      { label: 'Configurações', icon: 'pi pi-cog', routerLink: '/settings' },
+    ];
+  }
+}
