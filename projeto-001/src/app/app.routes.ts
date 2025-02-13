0afb84d45c434b36249a20f9b261d14aab9ada16
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-02-13 12:42:28 -0400

    Update: app routes

--- projeto-001/src/app/app.routes.ts
+++ projeto-001/src/app/app.routes.ts
@@ -2,9 +2,16 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 export const routes: Routes = [
-    {path: 'home',component: HomeComponent},
+    {
+        path: '',
+        component: LayoutComponent, // Aplica o layout à home
+        children: [
+          { path: 'home', component: HomeComponent }, // Home é renderizada dentro do layout
+        ]
+      },
     {path: 'login',component: LoginComponent},
     {path: 'register',component: RegisterComponent}
 ];
