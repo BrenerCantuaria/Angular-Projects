c48b36bdfc13898333d7daefbac50542301dfc35
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-02-14 11:05:20 -0400

    update: estrutura html e consumindo service

--- projeto-001/src/app/pages/home/home.component.ts
+++ projeto-001/src/app/pages/home/home.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table'; // Importe o TableModule
+import { TurmaAlunoService } from './home.service';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule,TableModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  turmas: any[] = [];
+  alunos: any[] = [];
 
+  constructor(private turmaAlunoService: TurmaAlunoService) {}
+
+  calcularMediaGeral(): number {
+    const total = this.alunos.reduce((sum, aluno) => sum + aluno.media, 0);
+    return total / this.alunos.length;
+  }
+  ngOnInit() {
+    this.turmas = this.turmaAlunoService.getTurmas();
+    this.alunos = this.turmaAlunoService.getAlunos();
+  }
 }
