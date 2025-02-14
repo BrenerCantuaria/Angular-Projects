c969a09e209d8085cfb181f888e0949e0916b6fd
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-02-14 10:58:56 -0400

    update: nova rota dashboard

--- projeto-001/src/app/app.routes.ts
+++ projeto-001/src/app/app.routes.ts
@@ -3,15 +3,18 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LayoutComponent } from './layout/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: LayoutComponent, // Aplica o layout à home
         children: [
-          { path: 'home', component: HomeComponent }, // Home é renderizada dentro do layout
+          { path: 'home', component: HomeComponent },// Home é renderizada dentro do layout
+          { path: 'dashboard', component: DashboardComponent}
         ]
-      },
+    },
+
     {path: 'login',component: LoginComponent},
     {path: 'register',component: RegisterComponent}
 ];
