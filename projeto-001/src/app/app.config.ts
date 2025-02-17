ab7030de1b2576ec7d8d6270cd1ef0dd7d0cb538
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-02-16 23:10:54 -0400

    add: dependencia de Animations Angular

--- projeto-001/src/app/app.config.ts
+++ projeto-001/src/app/app.config.ts
@@ -2,12 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
 import { MyPreset } from './preset/mypreset';
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations(),provideClientHydration(withEventReplay()),
     providePrimeNG({
       theme: {
           preset: MyPreset
