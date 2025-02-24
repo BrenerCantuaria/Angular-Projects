4457b40329232b4d4e2fe37b3325ff17e6c93a0c
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-02-24 11:27:28 -0400

    feat: implementando modal de cadastro de aluno

--- projeto-001/src/app/components/cadastrar-aluno-modal/cadastrar-aluno-modal.component.ts
+++ projeto-001/src/app/components/cadastrar-aluno-modal/cadastrar-aluno-modal.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastrar-aluno-modal',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, DialogModule, ButtonModule, InputTextModule, ReactiveFormsModule],
   templateUrl: './cadastrar-aluno-modal.component.html',
-  styleUrl: './cadastrar-aluno-modal.component.css'
 })
 export class CadastrarAlunoModalComponent {
+  @Output() alunoCadastrado = new EventEmitter<any>(); // Emite evento ao cadastrar aluno
+  displayModal = false;
+  alunoForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.alunoForm = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      dataNascimento: ['', Validators.required]
+    });
+  }
+
+  openModal() {
+    this.displayModal = true;
+  }
+
+  closeModal() {
+    this.displayModal = false;
+  }
+
+  cadastrarAluno() {
+    if (this.alunoForm.valid) {
+      const alunoData = { ...this.alunoForm.value };
+      this.alunoCadastrado.emit(alunoData); // Envia os dados do aluno para o componente pai
+      this.alunoForm.reset();
+      this.closeModal();
+    }
+  }
 }
