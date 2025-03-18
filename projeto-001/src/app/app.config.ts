b0df388c41014574cee7fa9180806e5a0f48365b
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-03-18 17:41:28 -0400

    feat: adiconando interceptors

--- projeto-001/src/app/app.config.ts
+++ projeto-001/src/app/app.config.ts
@@ -1,18 +1,32 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { providePrimeNG } from 'primeng/config';
 import { MyPreset } from './preset/mypreset';
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoginAuthInterceptor } from './services/login-auth.interceptor';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations(),provideClientHydration(withEventReplay()),
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+    provideClientHydration(withEventReplay()),
     providePrimeNG({
       theme: {
-          preset: MyPreset
-      }
-  })
-  ]
+        preset: MyPreset,
+      },
+    }),
+    provideHttpClient(withFetch()),
+    { provide: HTTP_INTERCEPTORS, useClass: LoginAuthInterceptor, multi: true },
+  ],
 };
