523ed396212a4fd00714c3348ada4bb0ad5874c3
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-03-23 13:21:19 -0400

    style: nova estrutura da sidebar

--- projeto-001/src/app/components/sidebar/sidebar.component.ts
+++ projeto-001/src/app/components/sidebar/sidebar.component.ts
@@ -1,26 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { MenuModule } from 'primeng/menu';
-import { CommonModule } from '@angular/common';
 import { Menubar } from 'primeng/menubar';
-// PrimeNG
-import { PanelMenuModule } from 'primeng/panelmenu';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { Ripple } from 'primeng/ripple';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss'],
-  imports: [CommonModule, MenuModule,PanelMenuModule],
+  standalone: true,
+  imports: [
+    Menubar,
+    BadgeModule,
+    AvatarModule,
+    InputTextModule,
+    Ripple,
+    CommonModule,
+  ],
 })
 export class SidebarComponent {
   items: MenuItem[] = [];
 
-  constructor() {
+  constructor(private router: Router) {
     this.items = [
       { label: 'Home', icon: 'pi pi-home', routerLink: '/' },
       { label: 'Turmas', icon: 'pi pi-users', routerLink: '/turmas' },
       { label: 'Alunos', icon: 'pi pi-id-card', routerLink: '/alunos' },
-      { label: 'Perfil', icon: 'pi pi-user', routerLink: '/perfil' },
-
     ];
+  }
+
+  getRouterUsuario() {
+    this.router.navigate(['/perfil']);
   }
 }
