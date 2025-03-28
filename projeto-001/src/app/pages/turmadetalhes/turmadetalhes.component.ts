3cc1ed5cff36776e9c224650d93fdc3f9e7d3718
BrenerCantuaria <BrenerCantuariaSantos@gmail.com>
2025-03-27 22:48:29 -0400

    feat: navegando de componentes da tela de detalhes

--- projeto-001/src/app/pages/turmadetalhes/turmadetalhes.component.ts
+++ projeto-001/src/app/pages/turmadetalhes/turmadetalhes.component.ts
@@ -1,15 +1,35 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { TabViewModule } from 'primeng/tabview';
-import { InputTextModule } from 'primeng/inputtext';
-import { RippleModule } from 'primeng/ripple';
+import { Component, ViewChild } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
-import { CalendarModule } from 'primeng/calendar'; // se você usar datas input futuramente
+import { AccordionModule } from 'primeng/accordion';
 import { BadgeModule } from 'primeng/badge';
 import { ButtonModule } from 'primeng/button';
-import { ViewChild } from '@angular/core';
+import { CalendarModule } from 'primeng/calendar'; // se você usar datas input futuramente
+import { InputTextModule } from 'primeng/inputtext';
+import { RippleModule } from 'primeng/ripple';
+import { TabViewModule } from 'primeng/tabview';
+
 import { CriarAtividadeModalComponent } from '../../components/criar-atividade-modal/criar-atividade-modal.component';
-import { CaretRightIcon } from 'primeng/icons';
+import { TurmaAlunoService } from '../home/home.service';
+
+interface IAtividade {
+  titulo: string;
+  prazo: Date | null;
+  publicado: Number | null;
+  status: string;
+  arquivo: string | null;
+  expanded: boolean;
+  descricao: string;
+}
+
+interface IAlunos {
+  nome: string;
+  turma: string;
+  status: string;
+  media: number;
+}
+
 @Component({
   selector: 'app-turma-detalhes',
   imports: [
@@ -21,28 +41,47 @@ import { CaretRightIcon } from 'primeng/icons';
     ButtonModule,
     BadgeModule,
     CalendarModule,
+    AccordionModule,
     CriarAtividadeModalComponent,
   ],
   templateUrl: './turmadetalhes.component.html',
   styleUrl: './turmadetalhes.component.css',
 })
 export class TurmadetalhesComponent {
-  tabSelecionado: 'mural' | 'atividades' | 'materiais' = 'mural';
+  listaDeAlunos: IAlunos[] = [];
+  constructor(private alunos: TurmaAlunoService) {}
+
+  carregarAluno() {
+    this.listaDeAlunos = this.alunos.getAlunos();
+  }
+
+  tabSelecionado: 'mural' | 'atividades' | 'materiais' | 'participantes' =
+    'mural';
   posts = [
     { titulo: 'Bem-vindos à turma!', data: new Date('2025-03-20') },
     { titulo: 'Primeiro material disponível.', data: new Date('2025-03-22') },
   ];
 
-  atividades = [
+  atividades: IAtividade[] = [
     {
       titulo: 'Lista 1 - Resistor e Ohm',
+      expanded: false,
+      publicado: Date.now(),
       prazo: new Date('2025-03-25'),
       status: 'Pendente',
+      arquivo: 'aula1.pdf',
+      descricao:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dicta fuga blanditiis cum modi consequuntur aliquam! Quia eligendi explicabo aspernatur consequuntur nihil ad quisquam cumque? Expedita enim quos odit facilis?',
     },
     {
       titulo: 'Mini-projeto: LED com Arduino',
       prazo: new Date('2025-03-28'),
+      publicado: Date.now(),
       status: 'Entregue',
+      arquivo: null,
+      expanded: false,
+      descricao:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dicta fuga blanditiis cum modi consequuntur aliquam! Quia eligendi explicabo aspernatur consequuntur nihil ad quisquam cumque? Expedita enim quos odit facilis?',
     },
   ];
 
@@ -58,6 +97,7 @@ export class TurmadetalhesComponent {
       link: 'https://exemplo.com/aula1-slides',
     },
   ];
+
   @ViewChild('modalAtividade') modalAtividade!: CriarAtividadeModalComponent;
 
   abrirModalAtividade() {
